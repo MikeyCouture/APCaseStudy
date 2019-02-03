8d27425dcc8f6344b2ba3da4fec78ed6cddebaee
MikeyCouture <michelcouture42@gmail.com>
2019-02-03 17:56:13 -0500

    Just polishing the code a bit. Everything is done, responsive, functionial and clean

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ const olaApp = {};
 
 olaApp.init = function (){
 
-    // window.addEventListener("scroll", () => {
-    //     const scrollable = document.documentElement.offsetHeight - document.documentElement.clientHeight;
-    //     const scrolled = Math.ceil(window.scrollY);
-
-    //     if (scrolled === scrollable){
-    //         runStats();
-    //     };
-    // });
-
+    // Grabbing the results stat div
     let allStats = document.querySelector(".results__content--stat");
-    
 
+    // Returning the position of stat div to see if it's in the viewport
     const isInViewport = function (elem) {
         let bounding = elem.getBoundingClientRect();
         return (
@@ -24,9 +16,8 @@ olaApp.init = function (){
         );
     };
 
-    
+    // listening for a scroll event
     let flag = true;
-    
     function newFunc(){
         window.addEventListener("scroll", () => {
             if(flag){
@@ -40,31 +31,13 @@ olaApp.init = function (){
 
     newFunc();
 
-
-
-
-    // const trigger = (function(){
-    //     let flag = true;
-    //     return function(){
-    //         if(flag === true){
-    //             window.addEventListener("scroll", () => {
-    //                 if (isInViewport(allStats)) {
-    //                     newStat();
-    //                 };
-    //             });
-    //             flag = false;
-    //         };
-    //     };
-    // });
-
+    // Firing off the runStats once allStats is in the viewport
     function newStat(){
         runStats();
     };
 
-
-
-
     function runStats(){
+        setTimeout(function(){
     // Grabbing the span inner text and setting new install variables
         let installStat = document.getElementById('statOne').innerText;
         let installStatTwo = document.getElementById('statTwo').innerText;
@@ -73,14 +46,14 @@ olaApp.init = function (){
         let installsTwo = 0;
         let installsThree = 0;
 
-        // A function that iterates on the page up to a given number
+        // Iterates number on the page
         function countingCost(){
             if (installs <= installStat){
                 setTimeout(function(){
                     document.getElementById('statOne').innerText = installs;
-                    installs = installs+1;
+                    installs = installs + 1;
                     countingCost();
-                }, 40);
+                }, 70);
             };
         };
 
@@ -90,7 +63,7 @@ olaApp.init = function (){
                     document.getElementById('statTwo').innerText = installsTwo;
                     installsTwo = installsTwo + 1;
                     countingInstalls();
-                }, 40);
+                }, 80);
             };
         };
 
@@ -100,7 +73,7 @@ olaApp.init = function (){
                     document.getElementById('statThree').innerText = installsThree;
                     installsThree = installsThree + 10;
                     countingReach();
-                }, 40);
+                }, 50);
             };
         };
 
@@ -108,6 +81,7 @@ olaApp.init = function (){
         countingInstalls();
         countingReach();
 
+        }, 400);
     };
 
 };
